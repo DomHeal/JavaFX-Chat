91b248987c38eead5326919c4a38c946481a4093
Dominic <Dominic.heal@europe.com>
2016-04-30 19:06:42 +0100

    Randomised direction, size and speed of each square on the login animation

--- client/src/main/java/com/client/login/LoginController.java
+++ client/src/main/java/com/client/login/LoginController.java
@@ -160,30 +160,60 @@ public class LoginController implements Initializable {
 
     public void generateAnimation(){
         Random rand = new Random();
-        int size = rand.nextInt(50) + 1;
+        int sizeOfSqaure = rand.nextInt(50) + 1;
+        int speedOfSqaure = rand.nextInt(10) + 5;
+        int startXPoint = rand.nextInt(420);
+        int startYPoint = rand.nextInt(350);
+        int direction = rand.nextInt(5) + 1;
 
-        Rectangle r1 = new Rectangle(0,0,size,size);
+
+        KeyValue moveXAxis = null;
+        KeyValue moveYAxis = null;
+        Rectangle r1 = null;
+
+        switch (direction){
+            case 1 :
+                // MOVE LEFT TO RIGHT
+                r1 = new Rectangle(0,startYPoint,sizeOfSqaure,sizeOfSqaure);
+                moveXAxis = new KeyValue(r1.xProperty(), 350 -  sizeOfSqaure);
+                break;
+            case 2 :
+                // MOVE TOP TO BOTTOM
+                r1 = new Rectangle(startXPoint,0,sizeOfSqaure,sizeOfSqaure);
+                moveYAxis = new KeyValue(r1.yProperty(), 420 - sizeOfSqaure);
+                break;
+            case 3 :
+                // MOVE LEFT TO RIGHT, TOP TO BOTTOM
+                r1 = new Rectangle(startXPoint,0,sizeOfSqaure,sizeOfSqaure);
+                moveXAxis = new KeyValue(r1.xProperty(), 350 -  sizeOfSqaure);
+                moveYAxis = new KeyValue(r1.yProperty(), 420 - sizeOfSqaure);
+                break;
+            case 4 :
+                // MOVE BOTTOM TO TOP
+                r1 = new Rectangle(startXPoint,420-sizeOfSqaure ,sizeOfSqaure,sizeOfSqaure);
+                moveYAxis = new KeyValue(r1.xProperty(), 0);
+                break;
+            case 5 :
+                // MOVE RIGHT TO LEFT
+                r1 = new Rectangle(420-sizeOfSqaure,startYPoint,sizeOfSqaure,sizeOfSqaure);
+                moveXAxis = new KeyValue(r1.xProperty(), 0);
+                break;
+            case 6 :
+                //MOVE RIGHT TO LEFT, BOTTOM TO TOP
+                r1 = new Rectangle(startXPoint,0,sizeOfSqaure,sizeOfSqaure);
+                moveXAxis = new KeyValue(r1.xProperty(), 350 -  sizeOfSqaure);
+                moveYAxis = new KeyValue(r1.yProperty(), 420 - sizeOfSqaure);
+                break;
+
+            default:
+                System.out.println("default");
+        }
+
         r1.setFill(Color.web("#F89406"));
         r1.setOpacity(0.1);
 
-        KeyValue xval;
-        if (size  % 2 == 0){
-            xval = new KeyValue(r1.xProperty(), 420);
-        } else {
-            xval = new KeyValue(r1.xProperty(), 420);
-        }
 
-        KeyValue yval;
-        if (size  % 3 == 0){
-            yval = new KeyValue(r1.yProperty(), 420);
-        } else {
-            yval = new KeyValue(r1.yProperty(), 420);
-        }
-
-        rand = new Random();
-        int speed = rand.nextInt(10) + 5;
-
-        KeyFrame keyFrame = new KeyFrame(Duration.millis(speed * 1000), xval, yval);
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(speedOfSqaure * 1000), moveXAxis, moveYAxis);
         Timeline timeline = new Timeline();
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.setAutoReverse(true);
